e7c900744d3a59e3d1b3c2c81e13a8d7e90b02c2
billow <billow.fun@gmail.com>
2021-03-18 03:00:19 +0800

    init

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,8 @@
+﻿module Accumulate
+
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list =
+    let rec helper (inp: 'a list) (acc: 'b list): 'b list =
+        match inp with
+        | [] -> acc
+        | head :: tail -> helper tail (func head :: acc)
+    helper input [] |> List.rev
---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,14 @@
+﻿module Raindrops
+
+let convert (number: int): string =
+    let folder state (k, v) =
+        if number % k = 0 then state + v else state
+
+    let notAny x =
+        if x = "" then number.ToString() else x
+
+    [ (3, "Pling")
+      (5, "Plang")
+      (7, "Plong") ]
+    |> List.fold folder ""
+    |> notAny
